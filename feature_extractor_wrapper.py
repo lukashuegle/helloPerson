f62be566d16220d4d942ed732b51ff826accba24
Tim Weber <weber.tim@hotmail.com>
2020-02-20 17:52:02 +0100

    Added image viewing

--- feature_extractor_wrapper.py
+++ feature_extractor_wrapper.py
@@ -2,46 +2,71 @@ from feature_extractor_class import feature_extraction
 from ringbuffer import Ringbuffer
 import logging
 import time
-import numbers as np
+import numpy as np
+from PIL import Image
+import cv2
+import threading
 
 THRESHHOLD_RE_ID = 0.65
 THRESHHOLD_NEW_ID = 1.05
 FEATURE_SIZE = 60
 
-def load_reID(model, batch_size):
-    logging.basicConfig(filename='feature_extractor_wrapper.log', level=logging.DEBUG)
-    feature_extractor = feature_extraction('0', model, '1', batch_size)
-    ringbuffer = Ringbuffer(200, 10000)
-    return feature_extractor, ringbuffer
+class feature_extractor_wrapper:
 
-def start_reID(feature_extractor, ringbuffer, img_array):
+    def __init__(self, model, batch_size):
+        self.last_image_shown = time.time() - 1000
+        self.model = model
+        self.batch_size = batch_size
+        self.threading_queue = []
+        image_show_thread = threading.Thread(target=self.image_viewer)
+        image_show_thread.start()
 
-    t_singleimg_start = time.time_ns()
-    feature_array = feature_extractor.extract_feature(img_array)
-    t_singleimg_end = time.time_ns()
-    logging.debug("Batch feature extraction took" + str((t_singleimg_end - t_singleimg_start)/1000000000) + "seconds")
-    #print(feature)
-    count = 0
-    for feature in feature_array:
-        feature = [feature]
+
+    def load_reID(self):
+        logging.basicConfig(filename='feature_extractor_wrapper.log', level=logging.DEBUG)
+        self.feature_extractor = feature_extraction('0', self.model, '1', self.batch_size)
+        self.ringbuffer = Ringbuffer(200, 10000)
+        #return feature_extractor, ringbuffer
+
+    def start_reID(self, img_array):
+
+        t_singleimg_start = time.time_ns()
+        feature_array = self.feature_extractor.extract_feature(img_array)
+        t_singleimg_end = time.time_ns()
+        logging.debug("Batch feature extraction took" + str((t_singleimg_end - t_singleimg_start)/1000000000) + "seconds")
         #print(feature)
-        smallest_index, smallest_distance = ringbuffer.nearestneighbors(feature)
-        print(smallest_distance)
-        logging.debug("Smallest distance: " + str(smallest_distance))
-        print(len(ringbuffer.ringbuffer))
-        logging.debug("Length of ringbuffer: " + str(len(ringbuffer.ringbuffer)))
-        if ringbuffer.ringbuffer:
-            logging.debug("Length of ringbuffer[0]" + str(len(ringbuffer.ringbuffer[0])))
-        if smallest_distance <= THRESHHOLD_RE_ID:
-            ringbuffer.addnewfeature(smallest_index, feature)
-            img_old = Image.fromarray(ringbuffer.getimage(smallest_index))
-            img_new = img_array[count]
-            img_new.show()
-            img_old.show()
-            #last_seen = ringbuffer.lastseen(smallest_index)
-            #print("Letztes mal gesehen: " + last_seen)
-            
-        elif smallest_distance >= THRESHHOLD_NEW_ID:
-            ringbuffer.addnewperson(feature, np.array(img_array[count]))
-            print("Willkommen!")
-        count += 1
+        count = 0
+        for feature in feature_array:
+            feature = [feature]
+            #print(feature)
+            smallest_index, smallest_distance = self.ringbuffer.nearestneighbors(feature)
+            print(smallest_distance)
+            logging.debug("Smallest distance: " + str(smallest_distance))
+            print(len(self.ringbuffer.ringbuffer))
+            logging.debug("Length of ringbuffer: " + str(len(self.ringbuffer.ringbuffer)))
+            if self.ringbuffer.ringbuffer:
+                logging.debug("Length of ringbuffer[0]" + str(len(self.ringbuffer.ringbuffer[0])))
+            if smallest_distance <= THRESHHOLD_RE_ID:
+                self.ringbuffer.addnewfeature(smallest_index, feature)
+                img_old = self.ringbuffer.getimage(smallest_index)
+                if (time.time() - self.last_image_shown) >= 5:
+                    self.update(img_old)
+                #last_seen = ringbuffer.lastseen(smallest_index)
+                #print("Letztes mal gesehen: " + last_seen)
+                
+            elif smallest_distance >= THRESHHOLD_NEW_ID:
+                self.ringbuffer.addnewperson(feature, np.array(img_array[count]))
+                print("Willkommen!")
+            count += 1
+
+    def update(self, image):
+        self.last_image_shown = time.time()
+        self.threading_queue.append(image)
+
+    def image_viewer(self):
+        img = np.array(Image.open("../../testdir/WIN_20200218_11_23_35_Pro (2).jpg"))
+        while True:
+            if len(self.threading_queue) > 0:
+                img = self.threading_queue.pop(0)
+            cv2.imshow("image", img)
+            cv2.waitKey(30)
