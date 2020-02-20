e8e05419bc4d2497bd7734038a2228550297971c
Lukas Hügle <luke@Lukass-MacBook-Pro.local>
2020-02-20 10:38:53 +0100

    fix dimensions in ringbuffer

--- ringbuffer.py
+++ ringbuffer.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from sklearn.neighbors import NearestNeighbors
 import collections
 import numpy as np
+import logging
 
 
 class Ringbuffer:
@@ -9,32 +10,31 @@ class Ringbuffer:
         self.ringbuffer = collections.deque(maxlen=personnumber)
         self.timebuffer = collections.deque(maxlen=personnumber)
         self.featurenumber = featurenumber
+        logging.basicConfig(filename='ringbuffer_class.log', level=logging.DEBUG)
 
     def addnewperson(self, featurearray):
         self.ringbuffer.append(featurearray)
         self.timebuffer.append(datetime.now().strftime("%H:%M:%S"))
 
     def addnewfeature(self, position, feature):
-        if len(self.ringbuffer[position])>= self.featurenumber:
-            print("ich bin hier drin")
+        if len(self.ringbuffer[position]) >= self.featurenumber:
             self.ringbuffer[position] = self.ringbuffer[position][1:self.featurenumber]
-        self.ringbuffer[position] = np.concatenate((np.array(self.ringbuffer[position]), np.array([feature])), axis=0)
+        self.ringbuffer[position] = np.concatenate((np.array(self.ringbuffer[position]), np.array(feature)), axis=0)
         self.timebuffer[position] = datetime.now().strftime("%H:%M:%S")
 
     def nearestneighbors(self, newefeature):
-        distance = np.arange(0)
-        for person in self.ringbuffer:
-            neigh = NearestNeighbors(n_neighbors=1)
-            print(self.ringbuffer)
-            neigh.fit(person)
-            distances, _ = neigh.kneighbors(newefeature)
-            distance = np.append(distance, np.amin(distances))
         smallestdistance = 100
         indexofsmallest = -1
-        if distance.size > 0:
-            smallestdistance = np.amin(distance)
-            indexofsmallest = (np.where(distance == smallestdistance))[0][0]
+        if self.ringbuffer:
+            distance = np.arange(0)
+            for person in self.ringbuffer:
+                neigh = NearestNeighbors(n_neighbors=1)
+                neigh.fit(person)
+                distances, _ = neigh.kneighbors(newefeature)
+                distance = np.append(distance, np.amin(distances))
+                smallestdistance = np.amin(distance)
+                indexofsmallest = (np.where(distance == smallestdistance))[0][0]
+        else:
+            logging.debug("Ringbuffer is empty")
         return indexofsmallest, smallestdistance
-
-
 
