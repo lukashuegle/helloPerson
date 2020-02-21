3cd8253133d8b051f6ceb805bd7f9f8d90d2ae11
Lukas <lhuegle@icloud.com>
2020-02-21 10:47:47 +0100

    update ringbuffer with persons id

--- ringbuffer.py
+++ ringbuffer.py
@@ -11,24 +11,27 @@ class Ringbuffer:
         self.timebuffer = collections.deque(maxlen=personnumber)
         self.imagebuffer = collections.deque(maxlen=personnumber)
         self.featurenumber = featurenumber
+        self.id = 0
         logging.basicConfig(filename='ringbuffer_class.log', level=logging.DEBUG)
 
     def addnewperson(self, featurearray, image):
         self.ringbuffer.append(featurearray)
-        self.timebuffer.append(datetime.now())
+        self.timebuffer.append([datetime.now(), self.id])
+        self.id = self.id + 1
         self.imagebuffer.append(image)
 
     def addnewfeature(self, position, feature):
         if len(self.ringbuffer[position]) >= self.featurenumber:
             self.ringbuffer[position] = self.ringbuffer[position][1:self.featurenumber]
         self.ringbuffer[position] = np.concatenate((np.array(self.ringbuffer[position]), np.array(feature)), axis=0)
-        self.timebuffer[position] = datetime.now()
+        # idold = self.timebuffer[position][1]
+        self.timebuffer[position][0] = datetime.now()
 
     def getimage(self, position):
         return self.imagebuffer[position]
 
     def lastseen(self, position):
-        return self.timebuffer[position]
+        return self.timebuffer[position][0], self.timebuffer[position][1]
 
     def nearestneighbors(self, newefeature):
         smallestdistance = 100
@@ -45,4 +48,3 @@ class Ringbuffer:
         else:
             logging.debug("Ringbuffer is empty")
         return indexofsmallest, smallestdistance
-
